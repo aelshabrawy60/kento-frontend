25564dcc73b1818178a30990a9dc34062060ee53
ahmed <aelshabrawy60@gmail.com>
2026-04-07 22:11:21 +0200

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
 import ClientLoginPage from '../pages/Client/ClientLoginPage'
 import './App.css'
 import ClientRegisterPage from '../pages/Client/ClientRegisterPage'
@@ -7,24 +7,31 @@ import ClientHomePage from '../pages/Client/ClientHomePage'
 import ClientNavbar from '../components/ClientNavbar'
 import VendorProfile from '../pages/Client/VendorProfile'
 
+function ClientLayout() {
+  return (
+    <div className='max-w-350 mx-auto p-4 md:p-6'>
+      <ClientNavbar />
+      <Outlet />
+    </div>
+  )
+}
+
 function App() {
-  
+
 
   return (
     <>
       <BrowserRouter>
-
-        <div className='max-w-[1400px] mx-auto p-4 md:p-6'>
-          <ClientNavbar/>
-          <Routes>
-            <Route path='/client/login' element={<ClientLoginPage/>}/>
-            <Route path='/client/register' element={<ClientRegisterPage/>}/>
-            <Route path='/client/onboard' element={<ClientOnboardPage/>}/>
-            <Route path='/client' element={<ClientHomePage/>}/>
-            <Route path='/client/vendors/:id' element={<VendorProfile/>}/>
-          </Routes>
-        </div>
-      </BrowserRouter>
+        <Routes>
+          <Route path='/client/login' element={<ClientLoginPage />} />
+          <Route path='/client/register' element={<ClientRegisterPage />} />
+          <Route path='/client/onboard' element={<ClientOnboardPage />} />
+          <Route element={<ClientLayout />}>
+            <Route path='/client' element={<ClientHomePage />} />
+            <Route path='/client/vendors/:id' element={<VendorProfile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter >
     </>
   )
 }
